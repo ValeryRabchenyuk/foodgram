350c36ff313e47b4699b025209aeaff0effac578
Valery Rabchenyuk <valery0623@gmail.com>
2025-01-27 19:38:39 +0300

    customize users admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UsersAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'username', 'email','first_name', 'last_name',)
+    search_fields = ('username', 'email')
+
+    # list_editable = ('is_staff',)
+    # list_filter = ('username',)
+    # list_display_links = ('id', 'username', 'email', 'full_name')
+
+# @admin.register(Follow)
+# class FollowAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'following',
+#         'follower',
+#     )
+#     search_fields = ('follower',)
