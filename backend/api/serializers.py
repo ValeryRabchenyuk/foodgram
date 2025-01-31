08dfc24d157b8e78d0f104bff4b2ff025652a215
Valery Rabchenyuk <valery0623@gmail.com>
2025-01-31 15:19:13 +0300

    Base64ImageField  serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,7 @@
 import re
+import base64
 # from rest_framework.serializers import ModelSerializer, SerializerMethodField
-
+from django.core.files.base import ContentFile
 from django.contrib.auth.tokens import default_token_generator
 
 from rest_framework import serializers
@@ -10,6 +11,18 @@ from djoser.serializers import UserCreateSerializer #UserSerializer
 # from .constants import EMAIL_MAX_LENGTH, USERNAME_MAX_LENGTH
 from ..users.models import User, Subscription
 from ..recipes.models import Tag
+
+
+class Base64ImageField(serializers.ImageField):
+
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
 
 
 class SignUpSerializer(UserCreateSerializer):
